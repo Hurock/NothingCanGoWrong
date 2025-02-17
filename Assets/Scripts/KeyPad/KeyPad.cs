b691a7bb7dcd4b687f451e64cf332fc4a2cd0e2a
MG2137 <mg2137v@gre.ac.uk>
2025-02-17 23:16:52 +0000

    WorkingKeyPadPuzzle
    You can click 4 random numbers and it will say that the password is incorrect. If you guess the correct password then it debugs that it was correct

---
+++ Assets/Scripts/KeyPad/KeyPad.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using UnityEngine;
+
+public class KeyPad : MonoBehaviour
+{
+    public string password = "1234";
+    private string userInput = "";
+
+    private void Start()
+    {
+        userInput = "";
+    }
+
+    public void ButtonClicked(string number)
+    {
+        userInput += number;
+        if (userInput.Length >= 4)
+        {
+            if(userInput == password)
+            {
+                Debug.Log("Password correct");
+            }
+            else
+            {
+                Debug.Log("Incorrect");
+                userInput = "";
+            }
+        }
+    }
+}
